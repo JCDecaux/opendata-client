6b6b2438cbacc6686c5bec114ef12d286a0be621
Romain Sertelon <romain@sertelon.fr>
2013-05-22 22:14:03 +0200

    Adds instance of the client

---
+++ src/main/java/fr/bluepyth/opendata/jcdecaux/client/vls/v1/VLSClient.java
@@ -0,0 +1,22 @@
+package fr.bluepyth.opendata.jcdecaux.client.vls.v1;
+
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.jboss.resteasy.client.jaxrs.engines.ApacheHttpClient4Engine;
+
+public class VLSClient {
+	
+	public static final VLSJCDResource vlsClient = getClient();
+	
+	private static VLSJCDResource getClient() {
+		ResteasyClient client = new ResteasyClientBuilder()
+			.httpEngine(new ApacheHttpClient4Engine(new DefaultHttpClient(new ThreadSafeClientConnManager()))).build();
+
+		ResteasyWebTarget target = client.target("https://api.jcdecaux.com/vls/v1");
+
+		return target.proxy(VLSJCDResource.class);
+	}
+}
