6e83aa9ea0290adba566ec94726f788cb721268d
Romain Sertelon <romain@sertelon.fr>
2015-02-20 22:43:12 +0100

    Use a builder to construct OpendataContext

--- opendata-client-common/src/main/java/com/jcdecaux/opendata/client/common/OpendataContext.java
+++ opendata-client-common/src/main/java/com/jcdecaux/opendata/client/common/OpendataContext.java
@@ -6,9 +6,29 @@ public class OpendataContext {
 
 	public static final String API_KEY_PARAM = "apiKey";
 
-	public final String apiKey;
+	public static Builder newBuilder() {
+		return new Builder();
+	}
+
+	public String apiKey;
 
 	public OpendataContext(String apiKey) {
 		this.apiKey = apiKey;
+	}
+
+	public static class Builder {
+		private String apiKey;
+
+		public Builder apiKey(String apiKey) {
+			this.apiKey = apiKey;
+			return this;
+		}
+
+		public OpendataContext build() {
+			if (apiKey == null)
+				throw new IllegalArgumentException("apiKey must be defined.");
+
+			return new OpendataContext(apiKey);
+		}
 	}
 }
