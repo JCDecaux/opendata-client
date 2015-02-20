0f6707b39ab678976f07497e91ceec214b1dbbfb
Romain Sertelon <romain@sertelon.fr>
2015-02-20 21:28:06 +0100

    Moves VLSResource into VLSClient

--- opendata-client/src/main/java/com/jcdecaux/opendata/client/vls/v1/VLSClient.java
+++ opendata-client/src/main/java/com/jcdecaux/opendata/client/vls/v1/VLSClient.java
@@ -1,5 +1,14 @@
 package com.jcdecaux.opendata.client.vls.v1;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.PoolingClientConnectionManager;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
@@ -7,7 +16,8 @@ import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.jboss.resteasy.client.jaxrs.engines.ApacheHttpClient4Engine;
 
-import com.jcdecaux.opendata.client.vls.v1.api.VLSResource;
+import com.jcdecaux.opendata.client.vls.v1.api.dto.ContractDTO;
+import com.jcdecaux.opendata.client.vls.v1.api.dto.StationDTO;
 
 public class VLSClient {
 
@@ -22,4 +32,23 @@ public class VLSClient {
 
         return target.proxy(VLSResource.class);
     }
+    
+    public interface VLSResource {
+
+        @GET
+        @Path("/contracts")
+        @Produces(MediaType.APPLICATION_JSON)
+        public List<ContractDTO> getContracts(@QueryParam("apiKey") String apiKey);
+
+        @GET
+        @Path("/stations")
+        @Produces(MediaType.APPLICATION_JSON)
+        public List<StationDTO> getStations(@QueryParam("apiKey") String apiKey, @QueryParam("contract") String contractName);
+
+        @GET
+        @Path("/stations/{number}")
+        @Produces(MediaType.APPLICATION_JSON)
+        public StationDTO getStation(@QueryParam("apiKey") String apiKey, @QueryParam("contract") String contractName, @PathParam("number") String stationNumber);
+    }
+
 }
