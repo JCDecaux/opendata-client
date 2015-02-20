0be058ac2b586eed2ace083a669a181bd4e95637
Romain Sertelon <romain@sertelon.fr>
2015-02-20 23:27:39 +0100

    Adds Spring Java Config

---
+++ opendata-client-spring/src/main/java/com/jcdecaux/opendata/client/spring/vls/v1/VLSV1Config.java
@@ -0,0 +1,10 @@
+package com.jcdecaux.opendata.client.spring.vls.v1;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class VLSV1Config {
+
+}
